1e7e90fda2a8c92802155efd11c24d9bbf2c209a
Lucas Teo <jawsandeur@gmail.com>
2025-04-20 00:36:48 +0800

    Revert "Change rollup config"
    This reverts commit 31c01acfd279c5ff45056a1b7fc745a1f113a21b.

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,12 @@ import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 
 export default {
-  plugins: [nodeResolve(), typescript()],
-  input: "src/index.ts",
-  output: {
-    plugins: [terser()],
-    dir: "dist",
-    format: "iife",
-    sourcemap: true,
-    globals: "console",
-  },
-};
+    plugins: [nodeResolve(), typescript()],
+    input: "src/index.ts",
+    output: {
+        plugins: [terser()],
+        dir: "dist",
+        format: "iife",
+        sourcemap: true,
+    }
+}
