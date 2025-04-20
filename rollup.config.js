50c80655c2723e0669825ac6df8d87ed54ae8f7a
Zheng Ze <zz.phua@gmail.com>
2025-04-20 15:23:45 +0800

    Attempt fix console not defined error in source academy

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,15 @@ import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 
 export default {
-    plugins: [nodeResolve(), typescript()],
-    input: "src/index.ts",
-    output: {
-        plugins: [terser()],
-        dir: "dist",
-        format: "iife",
-        sourcemap: true,
-    }
-}
+  plugins: [nodeResolve(), typescript()],
+  input: "src/index.ts",
+  output: {
+    plugins: [terser()],
+    dir: "dist",
+    format: "iife",
+    sourcemap: true,
+    globals: {
+      console: "console", // Map the external 'console' module to the browser's global 'console'
+    },
+  },
+};
